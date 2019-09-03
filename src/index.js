3ea41a470eaa08bbf8f5ddffacd18cdc956a8696
ggrifan <ggrifan>
2019-09-03 15:45:16 +0300

    Project structure

--- src/index.js
+++ src/index.js
@@ -1,44 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AppHeader = () => {
-    return <h1>ToDo List</h1>;
-};
-
-const SearchPanel = () => {
-    const searchText = 'Type here';
-    const searchStyle = {
-        fontSize: '20px'
-    };
-    return <input placeholder={searchText}
-                  style={searchStyle}
-                  className="foo"
-                  disabled />;
-};
-
-const TodoList = () => {
-    const list = ['Drink Coffee', 'Drink Whisky'];
-
-    return (
-        <ul>
-            <li>{ list[0] }</li>
-            <li>{ list[1] }</li>
-        </ul>
-    );
-}
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
 const App = () => {
-    let isLoggedIn = true;
-    const loginBox = <h3>Login Please</h3>;
-    const welcomeBox = <h3>Welcome Back</h3>;
-
-    const value = <script>alert("Boom!!!")</script>;
-
     return (
         <div>
-            { value }
-            { isLoggedIn ? welcomeBox : loginBox }
-            <h3>{ (new Date()).toString() }</h3>
             <AppHeader />
             <SearchPanel />
             <TodoList />
