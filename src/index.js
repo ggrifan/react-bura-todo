b040524b0a5a4772b83bf3b1bb7ac6d3e03d848e
ggrifan <ggrifan>
2019-09-03 15:02:35 +0300

    React components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-    <div>
-        <h1>ToDo List</h1>
-        <input placeholder="search" />
+const AppHeader = () => {
+    return <h1>ToDo List</h1>;
+};
+
+const SearchPanel = () => {
+    return <input placeholder="search" />;
+};
+
+const TodoList = () => {
+    return (
         <ul>
             <li>Learn React</li>
             <li>Goto Kipr</li>
         </ul>
-    </div>
-);
+    );
+}
 
-ReactDOM.render(el, document.getElementById('root'));
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel />
+            <TodoList />
+        </div>
+    );
+};
+    
+ReactDOM.render(<App />, document.getElementById('root'));
