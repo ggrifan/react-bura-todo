e0fc4901c2e3d703d65545f048a24e0c7170e48f
ggrifan <ggrifan>
2019-09-03 15:29:25 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -6,21 +6,39 @@ const AppHeader = () => {
 };
 
 const SearchPanel = () => {
-    return <input placeholder="search" />;
+    const searchText = 'Type here';
+    const searchStyle = {
+        fontSize: '20px'
+    };
+    return <input placeholder={searchText}
+                  style={searchStyle}
+                  className="foo"
+                  disabled />;
 };
 
 const TodoList = () => {
+    const list = ['Drink Coffee', 'Drink Whisky'];
+
     return (
         <ul>
-            <li>Learn React</li>
-            <li>Goto Kipr</li>
+            <li>{ list[0] }</li>
+            <li>{ list[1] }</li>
         </ul>
     );
 }
 
 const App = () => {
+    let isLoggedIn = true;
+    const loginBox = <h3>Login Please</h3>;
+    const welcomeBox = <h3>Welcome Back</h3>;
+
+    const value = <script>alert("Boom!!!")</script>;
+
     return (
         <div>
+            { value }
+            { isLoggedIn ? welcomeBox : loginBox }
+            <h3>{ (new Date()).toString() }</h3>
             <AppHeader />
             <SearchPanel />
             <TodoList />
