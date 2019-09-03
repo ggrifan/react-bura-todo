9dbb50076e9f11c589970c23f61992f4b1e92ca7
ggrifan <ggrifan>
2019-09-03 14:13:46 +0300

    React elements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello World</h1>;
+const el = (
+    <div>
+        <h1>ToDo List</h1>
+        <input placeholder="search" />
+        <ul>
+            <li>Learn React</li>
+            <li>Goto Kipr</li>
+        </ul>
+    </div>
+);
 
 ReactDOM.render(el, document.getElementById('root'));
