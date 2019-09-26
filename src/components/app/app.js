a6c88cb9c7e2f1c6ef2e34fe9d10d5b5f52212e3
ggrifan <ggrifan>
2019-09-26 11:31:10 +0300

    Delete item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,23 +7,34 @@ import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 import TodoList from '../todo-list';
 
-const App = () => {
-    const todos = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome React App', important: true, id: 2},
-        {label: 'Go To Cinema', important: false, id: 3},
-    ];
+export default class App extends React.Component {
+    state = {
+        todos: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome React App', important: true, id: 2},
+            {label: 'Go To Cinema', important: false, id: 3},
+        ]
+    }
 
-    return (
-        <div className="todo-app">
-            <AppHeader todo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SearchPanel />
-                <ItemStatusFilter />
+    deleteItem = (id) => {
+        this.setState(({ todos }) => {
+            const filtered = todos.filter((todo) => todo.id !== id);
+            return {
+                todos: filtered
+            };
+        });
+    }
+
+    render () {
+        return (
+            <div className="todo-app">
+                <AppHeader todo={1} done={3} />
+                <div className="top-panel d-flex">
+                    <SearchPanel />
+                    <ItemStatusFilter />
+                </div>
+                <TodoList todos={this.state.todos} onDeleted={ this.deleteItem }/>
             </div>
-            <TodoList todos={todos} onDeleted={ (id) => { console.log(`Deleted ${id}`) } }/>
-        </div>
-    );
+        );
+    }
 };
-
-export default App;
