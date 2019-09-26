f6885b21a8c75bdd8ddbfe8a7a6d9d4126c05078
ggrifan <ggrifan>
2019-09-26 10:31:43 +0300

    Event listener

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,6 +3,10 @@ import React from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends React.Component {
+    onLabelClick = () => {
+        console.log(`Done: ${this.props.label}`);
+    }
+
     render() {
         const {label, important = false} = this.props;
 
@@ -13,7 +17,8 @@ export default class TodoListItem extends React.Component {
         return (
             <span className="todo-list-item">
                 <span className="todo-list-item-label"
-                    style={ style }>
+                    style={ style }
+                    onClick={ this.onLabelClick }>
                     { label }
                 </span>
     
